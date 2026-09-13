342ab2699da49d6eb9d61556da3fae0e1c26da27
Thiago Dornelles <thiago.fdornelles@gmail.com>
2026-09-13 11:19:34 -0300

    perf: prerender landing page for CDN

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,8 @@
-import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  output: 'static',
   vite: {
     plugins: [tailwindcss()],
   },
